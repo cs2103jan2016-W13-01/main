7b29b44f016c34b4fdb543adef8143e69356e78d
qwerty2016 <tanfengji2009@gmail.com>
2016-03-08 19:51:17 +0800

    Added files via upload
    Newest version for UI

---
+++ src/Controller.java
@@ -0,0 +1,63 @@
+package gui;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.logic.Logic;
+
+
+public class Controller {
+	public static DisplayWindow DW;
+	
+	public static void main(String args[]){
+		DW = new DisplayWindow("Display");
+    	while (true){
+		executeCommand();
+		}}
+
+	public static void executeCommand(){
+		String cmd = DW.getCommand();
+/*		Logic.executeCmd(cmd);*/
+	}	
+	
+	public static void displayTask(String task){
+		DW.displayTask(task);
+	}
+
+	public static void clearTextArea(){
+		DW.clearWindow();	
+	}
+	}
+
+/**	public static void displayTasks(){
+		clearTextArea();
+		File storageFile = Storage.retriveFile();
+		displayFromStorage(storage);
+	}
+	
+	public static void displayFromStorage(File storageFile) {	
+		int linesWritten = 0;
+		try {	
+			BufferedReader displayBufferedReader = initBufferedReader(storageFile);
+			String line = "";
+			while ((line = displayBufferedReader.readLine()) != null) {
+				DW.displayTask(linesWritten+ ". " +line + "\n");
+				linesWritten++;
+				}
+			displayBufferedReader.close();
+		}
+		catch (IOException e) {
+		}	
+	}
+	
+	private static BufferedReader initBufferedReader(File textFile) {
+		try {
+			FileReader fileReader = new FileReader(textFile.getAbsoluteFile());
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			return bufferedReader;
+		}
+		catch (IOException e) {
+		}
+		return null;
+	}}**/
