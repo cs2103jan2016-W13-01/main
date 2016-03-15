c95a400859f3a2177df11a12fc3049ea07a7a8af
Denniskzq <denniskohzhiquan@gmail.com>
2016-03-16 01:21:51 +0800

    no message

--- src/logic/CommandDetails.java
+++ src/logic/CommandDetails.java
@@ -3,38 +3,29 @@ package logic;
 public class CommandDetails {
 	private CommandType cmd;
 	private Task task;
-<<<<<<< HEAD
 
 	private int taskNumber;
 
-=======
-	private int inputNum;
->>>>>>> a4fb3a2bda54b2d0b98ca12255f9b79d62744ba8
 
 	public CommandDetails(CommandType cmd, Task task, int taskNumber) {
 		
 		this.cmd = cmd;
 		this.task = task;
-		this.inputNum = taskNumber;
+		this.taskNumber = taskNumber;
 	}
 	
-<<<<<<< HEAD
+	public CommandDetails(CommandType cmd, Task task){
 
-	public CommandDetails(CommandType cmd, Task task){
-		
-=======
-	public CommandDetails(CommandType cmd, Task task){
->>>>>>> a4fb3a2bda54b2d0b98ca12255f9b79d62744ba8
 		this.cmd=cmd;
 		this.task=task;
-		inputNum = 0;
+		this.taskNumber = 0;
 	}
 	
 	public CommandDetails(CommandType cmd, int taskNumber) {
 		
 		this.cmd = cmd;
 		this.task = null;
-		this.inputNum = taskNumber;
+		this.taskNumber = taskNumber;
 	}
 	
 	public CommandType getCommand() {
@@ -44,21 +35,9 @@ public class CommandDetails {
 	public Task getTask() {
 		return task;
 	}
-<<<<<<< HEAD
-	
 
 	public int getTaskNumber() {
 		return taskNumber;
 	}
-=======
 
-	public void setInputNum(int inputNum) {
-		this.inputNum = inputNum;
-	}
-	public int getInputNum() {
-		return inputNum;
-	}
-
->>>>>>> a4fb3a2bda54b2d0b98ca12255f9b79d62744ba8
-	
 }
