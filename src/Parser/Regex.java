0cc06315c616abd92d63ac2854d27e329e106962
Denniskzq <denniskohzhiquan@gmail.com>
2016-04-06 21:41:27 +0800

    slight changes to misc regex

--- src/Parser/Regex.java
+++ src/Parser/Regex.java
@@ -6,18 +6,15 @@ public class Regex {
 	static final String REGEX_SPACE = "(\\s+)";
 	static final String START = "(?<=^|\\s)";
 	static final String END = "(?=\\s|$)";
-	static final String MISC_REGEX ="(at?|from?|to?|till? |until?|by?|at?|on?|next?|this?|by next?|by this?)"+REGEX_SPACE;
+	static final String MISC_REGEX ="(at?|from?|to?|till?|until?|by?|on?|next?|this?|by next?|by this?)";
 	static final String MONTHNAME = "(jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?)";
 	static final String DATES = "(\\d?\\d)(?:st|rd|nd|th)?";
 	static final String YEAR = "(\\d{2}|\\d{4})?";
-
 	static final String DATE_MONTH_REGEX = "(?<=\\s|^)" + DATES + REGEX_SPACE + MONTHNAME + END;
 	static final String MONTH_DATE_REGEX = "(?<=\\s|^)" + MONTHNAME +REGEX_SPACE + DATES + END;
 	static final String DATE_MONTH_YEAR_REGEX = "(?<=\\s|^)" + DATES + REGEX_SPACE+ MONTHNAME + REGEX_SPACE + YEAR + END;
 	static final String MONTH_DATE_YEAR_REGEX = "(?<=\\s|^)" + MONTHNAME +REGEX_SPACE + DATES + REGEX_SPACE + YEAR + END;
-
 	static final String TMR_REGEX = "(today?|tmr?|tomorrow?)";
-
 	static final String DAYNAME = "(next week?|next month?|mon(?:day)?|tue(?:sday)?|wed(?:nesday)?|thur(?:sday)?|fri(?:day)?|sat(?:urday)?|sun(?:day)?)";
 	static final String TWELVE_HR_REGEX = "([0-9]?[0-9])([.:][0-9][0-9])?\\s?(am|pm)";
 	static final String TWENTYFOUR_HR_REGEX = "(([0-9]?[0-9])[:]([0-9][0-9]))\\s?[?:h|H]?";
