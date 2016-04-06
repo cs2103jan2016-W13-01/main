2fa0e6df04524ff4427bfd1c4e80cf72dca852b4
Leow Wei Siang <leow_weisiang@live.com.sg>
2016-04-06 10:58:43 +0800

    Added some test cases for add, delete, edit, undo, and redo. More to be added.

---
+++ Test/testEdit.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class testEdit {
+
+	//@@author: A0134185H
+			
+	@Test
+	public void test() throws IOException {
+		/*
+		String output = FileUtils.readFileToString(IntegratedTestRunner.runTest("in1.txt"), "UTF-8");
+		String expected = FileUtils.readFileToString(IntegratedTestRunner.getExpectedFile("in1.txt.expected"), "UTF-8");
+		assertEquals(expected, output);
+		*/
+		IntegratedTestRunner.runTest("in4.txt");
+	}
+}
